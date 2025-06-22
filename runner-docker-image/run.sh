8cd03cbc611bf6f8118adc57338fb0f49a67f6b2
Anil <anil@test.com>
2025-06-22 19:56:57 +0530

    Learn-github actions

--- runner-docker-image/run.sh
+++ runner-docker-image/run.sh
@@ -1,10 +1,4 @@
 #./config.sh --unattended --url https://github.com/anilkumarm429 --token AQ7WUZO4O5FOI6BWRRLNJ2DILAI2I --name container-1 --runnergroup default --work _work
-#if [ -z "${ORG}" ]; then echo Input ORG is missing  ; exit 1 ; fi
-#if [ -z "${TOKEN}" ]; then echo Input TOKEN is missing  ; exit 1 ; fi
-#if [ -z "${NAME}" ]; then echo Input NAME is missing  ; exit 1 ; fi
-#
-#./config.sh --unattended --url ${ORG} --token %{TOKEN} --name ${NAME} --runnergroup default --work _work
-#./run.sh
 
 if [ -z "${ORG}" ]; then echo Input ORG is missing ; exit 1 ; fi
 if [ -z "${TOKEN}" ]; then echo Input TOKEN is missing ; exit 1 ; fi
